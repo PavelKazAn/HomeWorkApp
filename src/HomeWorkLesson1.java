123f4a3a84a18eccce493f4e1e571e728745fbc7
Pavel <Frozzty@yandex.ru>
2021-10-06 13:04:29 +0300

    Немного упростил код

--- src/HomeWorkLesson1.java
+++ src/HomeWorkLesson1.java
@@ -23,7 +23,7 @@ public class HomeWorkLesson1 {
 
         if (value<=0){
             System.out.println("Красный");
-        }else if(value>0 && value<=100){
+        }else if(value<=100){
             System.out.println("Желтый");
         }else{
             System.out.println("Зеленый");
